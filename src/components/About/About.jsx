c4a4fa9533af55ebccac0c15fca46f472782c922
ZyanneM <92530551+ZyanneM@users.noreply.github.com>
2023-04-08 22:24:34 +0200

    add about content

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -4,9 +4,9 @@ import './About.css'
 const About = () => {
     return (
         <div className="about">
-            <h1>A propos de moi</h1>
-            <h2>Qui suis-je ?</h2>
-            <h2>Skills</h2>
+            <h2><span>Once upon a time</span><br></br>a girl who like to build websites</h2>
+            <p>I fall into the exciting history of web development in 2021 and decided to make it my profession. I enjoy building visual identities and I aim to enhance my skills to develop interactive websites and be able to play with 3D experiences.</p>
+            {/* <div className='photo'></div> */}
         </div>
     );
 };
--- src/components/Homepage/Homepage.jsx
+++ src/components/Homepage/Homepage.jsx
@@ -3,10 +3,20 @@ import './Homepage.css'
 
 const Homepage = () => {
     return (
-        <div>
+        <div className='homepage'>
             <div className='home'></div>
-            <h1>Marilyn Olejniczak</h1>
-            <h1>Développeuse Front-End</h1>
+            <div className='reseaux'>
+                <div className='linkedin'></div>
+                <div className='github'></div>
+                <div className='follow'>
+                    <div className='lign'></div>
+                    <p>Follow-Me</p>
+                </div>
+            </div>
+            <div className='hometext'>
+                <h1>Marilyn Olejniczak</h1>
+                <h1>Front-End Developer</h1>
+            </div>
         </div>
     );
 };
