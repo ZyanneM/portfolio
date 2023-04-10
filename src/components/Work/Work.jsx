6248b751b5ecd01dd513b9179e093efb257948d8
ZyanneM <92530551+ZyanneM@users.noreply.github.com>
2023-04-10 21:12:54 +0200

    try to add slider

--- src/components/Work/Work.jsx
+++ src/components/Work/Work.jsx
@@ -3,11 +3,36 @@ import './Work.css'
 import SingleWork from '../SingleWork/SingleWork';
 
 const Work = () => {
+const projects = ["un", "deux", "trois"]
+
     return (
         <div className='work-container'>
             <h1>My web development adventures</h1>
             {/* <p>Put here an awesome projects gallery</p> */}
-            <SingleWork/>
+            <div className='slides-container'>
+                {projects.map((project, index) => (
+                    <>
+                    <div
+                    className={`work-card num${index}`}>
+                    <SingleWork
+                    key={project}
+                />
+                </div>
+                </>
+                ))}
+            </div>
+            <div className="slider-navigation">
+                    <button className="slider-nav prev">&lsaquo;</button>
+                    <button className="slider-nav next">&rsaquo;</button>
+                </div>  
+            <div className="slider-dots">
+                        <ul>
+                        {projects.map((project) => ( 
+                            <li className='active'></li>
+                ))}
+                        </ul>
+                    </div>
+            
         </div>
     );
 };
